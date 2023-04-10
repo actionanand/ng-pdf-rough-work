bba8aa0718592b6062b5fa242caf4d88807e30e1
Anand raja <meetanandns@gmail.com>
2023-04-10 10:34:48 +0000

    injectable DI

--- src/app/services/pdf.service.ts
+++ src/app/services/pdf.service.ts
@@ -1,20 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PdfService {
-  constructor(private http: HttpClient) {}
+  // constructor(private http: HttpClient) {}
+
+  private http = inject(HttpClient);
 
   downloadPdf(url: string): Observable<Blob> {
-    return this.http.get(url, { responseType: 'blob' }).pipe(
-      map((result: Blob) => {
-        return result;
-      }),
+    return inject(HttpClient)
+      .get(url, { responseType: 'blob' })
+      .pipe(
+        map((result: Blob) => {
+          return result;
+        }),
+      );
+  }
+
+  getPlayersAsPromise(url: string): Promise<Blob> {
+    return lastValueFrom(
+      inject(HttpClient)
+        .get(url, { responseType: 'blob' })
+        .pipe(
+          map((result: Blob) => {
+            return result;
+          }),
+        ),
     );
   }
 
