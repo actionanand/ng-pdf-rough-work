1fbbb0808e731406491ffd7047b4310700e1fa7e
Anand raja <meetanandns@gmail.com>
2023-04-10 12:50:08 +0000

    fixed issues

--- src/app/services/pdf.service.ts
+++ src/app/services/pdf.service.ts
@@ -2,35 +2,19 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
-import { Observable, lastValueFrom } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PdfService {
-  // constructor(private http: HttpClient) {}
-
-  private http = inject(HttpClient);
+  constructor(private http: HttpClient) {}
 
   downloadPdf(url: string): Observable<Blob> {
-    return inject(HttpClient)
-      .get(url, { responseType: 'blob' })
-      .pipe(
-        map((result: Blob) => {
-          return result;
-        }),
-      );
-  }
-
-  getPlayersAsPromise(url: string): Promise<Blob> {
-    return lastValueFrom(
-      inject(HttpClient)
-        .get(url, { responseType: 'blob' })
-        .pipe(
-          map((result: Blob) => {
-            return result;
-          }),
-        ),
+    return this.http.get(url, { responseType: 'blob' }).pipe(
+      map((result: Blob) => {
+        return result;
+      }),
     );
   }
 
