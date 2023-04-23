e199601adff0a4f31bc3098f4444c94d24459d47
Anand raja <meetanandns@gmail.com>
2023-04-23 12:08:58 +0000

    working pdf

--- src/app/pages/pdf-test/pdf-test.component.ts
+++ src/app/pages/pdf-test/pdf-test.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject, OnInit, VERSION } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
 
 import { getDocument, GlobalWorkerOptions, version } from 'pdfjs-dist';
 
 import { getHost } from '../../shared/functions/get-host';
-import { pdfBase64 } from '../../../assets/asset-file/sample-pdf-base64';
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -31,73 +30,45 @@ export class PdfTestComponent implements OnInit {
     this.loadPDF();
   }
 
-  loadPDF() {
+  async loadPDF() {
     const loadingTask = getDocument(this.pdfurl);
 
-    loadingTask.promise.then((pdf) => {
-      console.log('pdf loaded: ', pdf);
+    const pdf = await loadingTask.promise;
+    console.log('full pdf loaded: ', pdf);
 
-      // Get div#container and cache it for later use
-      const container: HTMLElement = this.document.querySelector('#container') as HTMLElement;
+    // Get div#container and cache it for later use
+    const container: HTMLElement = this.document.querySelector('#container') as HTMLElement;
 
-      // Loop from 1 to total_number_of_pages in PDF document
-      for (let i = 1; i <= pdf.numPages; i++) {
-        pdf.getPage(i).then((page) => {
-          let scale = 1.5;
-          let viewport = page.getViewport({ scale });
+    // Loop from 1 to total_number_of_pages in PDF document
+    for (let i = 1; i <= pdf.numPages; i++) {
+      const page = await pdf.getPage(i);
+      const scale = 1.5;
+      const viewport = page.getViewport({ scale });
+      const div = document.createElement('div');
 
-          let div = document.createElement('div');
+      // Set id attribute with page-#{pdf_page_number} format
+      div.setAttribute('id', 'page-' + (page._pageIndex + 1));
 
-          // Set id attribute with page-#{pdf_page_number} format
-          div.setAttribute('id', 'page-' + (page._pageIndex + 1));
+      // This will keep positions of child elements as per our needs
+      div.setAttribute('style', 'position: relative');
+      container.appendChild(div);
 
-          // This will keep positions of child elements as per our needs
-          div.setAttribute('style', 'position: relative');
+      const canvas = document.createElement('canvas');
 
-          container.appendChild(div);
+      // Append Canvas within div#page-#{pdf_page_number}
+      div.appendChild(canvas);
 
-          let canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d')!;
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
 
-          // Append Canvas within div#page-#{pdf_page_number}
-          div.appendChild(canvas);
+      const renderContext = {
+        canvasContext: context,
+        viewport: viewport,
+      };
 
-          let context = canvas.getContext('2d')!;
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-
-          let renderContext = {
-            canvasContext: context,
-            viewport: viewport,
-          };
-
-          // Render PDF page
-          page.render(renderContext);
-        });
-      }
-
-      // Fetch the first page
-      // let pageNumber = 1;
-      // pdf.getPage(pageNumber).then((page) => {
-      //   console.log('first page is loaded');
-
-      //   // Prepare canvas using PDF page dimensions
-      //   // const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
-      //   // container.height = viewport.height;
-      //   // container.width = viewport.width;
-
-      //   // Render PDF page into canvas context
-      //   // const renderContext = {
-      //   //   canvasContext: context,
-      //   //   viewport,
-      //   // };
-
-      //   // const renderTask = page.render(renderContext);
-
-      //   // renderTask.promise.then(() => {
-      //   //   if (pdf != null) pdf.destroy();
-      //   //   console.log('Page rendered in the canvas as image');
-      //   // });
-      // });
-    });
+      // Render PDF page
+      page.render(renderContext);
+    }
   }
 }
