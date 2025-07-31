41aad0d9bcbc6b03e5020d87c949ea628747f181
Anand raja <meetanandns@gmail.com>
2025-07-31 12:58:07 +0000

    pdf url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   backend: {
-    remotePDF: 'https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK',
-    localPdf: 'https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK',
+    remotePDF: 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf',
+    localPdf: 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf',
     players: 'https://www.balldontlie.io/api/v1/players',
-    gitHubPdf: 'https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK',
+    gitHubPdf: 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf',
   },
 };
